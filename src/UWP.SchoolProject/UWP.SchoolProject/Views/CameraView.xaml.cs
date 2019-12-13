5a5d86c7bd05a43d26b1bdd65125f6173c07bbb2
Olof <olssonolof@hotmail.com>
2019-12-13 11:48:36 +0100

    Removed comments

--- src/UWP.SchoolProject/UWP.SchoolProject/Views/CameraView.xaml.cs
+++ src/UWP.SchoolProject/UWP.SchoolProject/Views/CameraView.xaml.cs
@@ -20,13 +20,10 @@ using Windows.UI.Xaml.Media.Imaging;
 using System.Threading.Tasks;
 using UWP.SchoolProject.ViewModels;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace UWP.SchoolProject.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class CameraView : Page
     {
         private CameraViewModel ViewModel;
