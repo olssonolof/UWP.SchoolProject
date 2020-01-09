074268e46d2063da91aa112f56aad2c2f5cf0845
Olof <olssonolof@hotmail.com>
2020-01-09 10:22:45 +0100

    Removed comments

--- src/UWP.SchoolProject/UWP.SchoolProject/MainPage.xaml.cs
+++ src/UWP.SchoolProject/UWP.SchoolProject/MainPage.xaml.cs
@@ -6,10 +6,6 @@ using Windows.UI.Xaml.Controls;
 
 namespace UWP.SchoolProject
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    /// 
 
     public sealed partial class MainPage : Page
     {
